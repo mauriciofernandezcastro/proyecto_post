5db53f8d145d45d2ffcb77da4a130922d68d26de
Mauricio Fernandez <mauricio.fernandezcastro@gmail.com>
2023-07-21 20:31:03 -0400

    v3 corregir routes.rb, post_controller.rb, index.html.erb

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,19 +1,26 @@
 class PostController < ApplicationController
   def index
+    
   end
 
   def create
-    @Post = Post.new(posts_params)
-    @Post.save
-    redirect_to 'post/resultado'
-
+    @post = Post.new(posts_params)
+    if @post.save
+    redirect_to post_resultado_path
+    else
+      render 'resultado'
+    end
   end
 
   def resultado
+    @last_post = Post.last
   end
 
   def ver
+    
   end
+
+  private
 
   def posts_params
     params.require(:post).permit(:author, :date, :content, :image)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'post/index'
   get 'post/resultado'
   get 'post/ver'
-  post 'crearpost' => 'post/create'
+  post 'post/seaharrier', to: 'post#create', as: 'post_create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
